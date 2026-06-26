84dd04dd4c8ae67880c7442bacff3ed6dc9ac0c8
George Chitechi <mctapplications@gmail.com>
2026-06-27 02:36:07 +0300

    Initial Commit

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,16 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::get('dashboard', \App\Livewire\Dashboard::class)
+Route::livewire('dashboard', \App\Livewire\Dashboard::class)
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
-    Route::get('settings/profile', Profile::class)->name('settings.profile');
-    Route::get('settings/password', Password::class)->name('settings.password');
-    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+    Route::livewire('settings/profile', Profile::class)->name('settings.profile');
+    Route::livewire('settings/password', Password::class)->name('settings.password');
+    Route::livewire('settings/appearance', Appearance::class)->name('settings.appearance');
 });
 
 require __DIR__ . '/auth.php';
