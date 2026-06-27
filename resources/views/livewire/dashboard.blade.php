552fce44b58186087295cde6a08a3bfaaa188bea
George Chitechi <mctapplications@gmail.com>
2026-06-27 03:11:58 +0300

    Initial Commit

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -2,24 +2,24 @@
     <x-layouts.sidebar />
 </x-slot:sidebar>
 
-<div class="container-fluid p-4">
+<div class="container-fluid p-3">
     <!-- Top Row Placeholders -->
-    <div class="row g-4 mb-4">
+    <div class="row g-3 mb-3">
         <div class="col-12 col-lg-4">
-            <div class="border rounded-4 placeholder-hatch w-100" style="min-height: 220px; border-color: rgba(var(--bs-body-color-rgb), 0.05) !important; border-width: 1px !important;"></div>
+            <div class="border rounded-3 placeholder-hatch w-100" style="min-height: 220px; border-color: rgba(var(--bs-body-color-rgb), 0.05) !important; border-width: 1px !important;"></div>
         </div>
         <div class="col-12 col-lg-4">
-            <div class="border rounded-4 placeholder-hatch w-100" style="min-height: 220px; border-color: rgba(var(--bs-body-color-rgb), 0.05) !important; border-width: 1px !important;"></div>
+            <div class="border rounded-3 placeholder-hatch w-100" style="min-height: 220px; border-color: rgba(var(--bs-body-color-rgb), 0.05) !important; border-width: 1px !important;"></div>
         </div>
         <div class="col-12 col-lg-4">
-            <div class="border rounded-4 placeholder-hatch w-100" style="min-height: 220px; border-color: rgba(var(--bs-body-color-rgb), 0.05) !important; border-width: 1px !important;"></div>
+            <div class="border rounded-3 placeholder-hatch w-100" style="min-height: 220px; border-color: rgba(var(--bs-body-color-rgb), 0.05) !important; border-width: 1px !important;"></div>
         </div>
     </div>
 
     <!-- Bottom Row Placeholder -->
     <div class="row">
         <div class="col-12">
-            <div class="border rounded-4 placeholder-hatch w-100" style="min-height: 500px; border-color: rgba(var(--bs-body-color-rgb), 0.05) !important; border-width: 1px !important;"></div>
+            <div class="border rounded-3 placeholder-hatch w-100" style="min-height: 500px; border-color: rgba(var(--bs-body-color-rgb), 0.05) !important; border-width: 1px !important;"></div>
         </div>
     </div>
 </div>
